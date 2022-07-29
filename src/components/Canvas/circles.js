bae8c2baf20ae107a1ae0580774d910a2f8b5b5f
dchueri <diegochueri@gmail.com>
2022-07-29 09:38:32 -0400

    circles - eye

--- src/components/Canvas/circles.js
+++ src/components/Canvas/circles.js
@@ -11,14 +11,34 @@ const sketch = () => {
     context.fillStyle = "#090514";
     context.fillRect(0, 0, width, height);
 
-    const xCenter = 0;
-    const yCenter = 0;
+    const xCenter = width * 0.5;
+    const yCenter = height * 0.5;
     const w = width * 0.01;
-    const h = height * 10.0;
+    const h = height * 0.2;
     let x, y;
 
-    const numberOfItems = 20;
+    const numberOfItems = 35;
     const radius = width * 0.3;
+
+    for (let i = 0; i < numberOfItems; i++) {
+      const slice = math.degToRad(360 / numberOfItems);
+      const angle = slice * i;
+
+      x = xCenter + radius * Math.sin(angle);
+      y = yCenter + radius * Math.cos(angle);
+
+      context.save();
+      context.translate(xCenter, yCenter);
+      context.rotate(-angle);
+
+      context.lineWidth = random.range(5, 20);
+
+      context.beginPath();
+      context.arc(0, 0, radius * random.range(0, 2), slice * random.range(5, -50), slice * random.range(1, 4));
+      context.strokeStyle = "#1D0F42";
+      context.stroke();
+      context.restore();
+    }
 
     for (let i = 0; i < numberOfItems; i++) {
       const slice = math.degToRad(360 / numberOfItems);
@@ -45,7 +65,7 @@ const sketch = () => {
       context.lineWidth = random.range(5, 20);
 
       context.beginPath();
-      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -5), slice * random.range(1, 4));
+      context.arc(0, 0, radius * random.range(0, 2.3), slice * random.range(5, -5), slice * random.range(1, 4));
       context.strokeStyle = "#BF0656";
       context.stroke();
       context.restore();
